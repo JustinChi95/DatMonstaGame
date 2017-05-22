790632bfc1453fe1ccb5ff4f04f335bfe020d73b
JustinChi95 <justin.chi@yahoo.com>
2017-05-22 11:13:10 -0700

    Creation017

---
+++ Source/DatMonstaGame/DatMonstaGameGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "DatMonstaGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DATMONSTAGAME_API ADatMonstaGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
